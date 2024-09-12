3ab368ae42a1f91add5abfb752b4704e916a0e03
Lee Morgan <lee@leemorgan.dev>
2024-09-12 16:31:45 -0400

    Create the main hero image.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "../global.css";
+
+    import heroImg from "$lib/hero.webp";
+</script>
+
+<div class="container">
+    <div class="hero">
+        <img src={heroImg} alt="Sunrise over Murrells Inlet">
+
+        <h1>Inlet Sites</h1>
+
+        <h2>Professional Website Services</h2>
+    </div>
+</div>
+
+<style>
+    .container{
+        height: 100%;
+        width: 100%;
+    }
+
+    .hero{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%; 
+        height: 100vh;
+        position: relative;
+    }
+
+    .hero img{
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        right: 0;
+        z-index: -1;
+    }
+
+    .hero h1{
+        font-size: 68px;
+        color: white;
+    }
+
+    .hero h2{
+        color: white;
+    }
+</style>
