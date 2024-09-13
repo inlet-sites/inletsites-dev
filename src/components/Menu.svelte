b6aa187e754914fca1a3460abf0ec0115d1dec6b
Lee Morgan <lee@leemorgan.dev>
2024-09-13 18:29:24 -0400

    Update the menu for mobile.

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,13 +1,54 @@
-<div class="menu">
-    <a class="inletSites" href="/">Inlet Sites</a>
-    <a href="/services">Services</a>
-    <a href="/prices">Prices</a>
-    <a href="/process">The Process</a>
-    <a href="/contact">Contact</a>
-    <a href="/about">About</a>
+<script>
+    let displayMenu = false;
+
+    const toggleMenu = ()=>{
+        displayMenu = !displayMenu;
+    }
+</script>
+
+<div class="openMenu" on:click={toggleMenu}>
+    <svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" color="#000000">
+        <path d="M3 5H21" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+        <path d="M3 12H21" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+        <path d="M3 19H21" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+    </svg>
 </div>
 
+{#if displayMenu}
+    <div class="closeMenu" on:click={toggleMenu}>
+        <svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" color="#000000">
+            <path d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+        </svg>
+    </div>
+{/if}
+
+{#if displayMenu}
+    <div class="menu" on:click={toggleMenu}>
+        <a class="inletSites" href="/">Inlet Sites</a>
+        <a href="/services">Services</a>
+        <a href="/prices">Prices</a>
+        <a href="/process">The Process</a>
+        <a href="/contact">Contact</a>
+    </div>
+{/if}
+
 <style>
+    .openMenu{
+        display: none;
+        position: absolute;
+        top: 35px;;
+        left: 35px;
+        z-index: 2;
+    }
+
+    .closeMenu{
+        display: none;
+        position: absolute;
+        top: 35px;
+        right: 35px;
+        z-index: 4;
+    }
+
     .menu{
         display: flex;
         justify-content: flex-end;
@@ -50,19 +91,33 @@
         background: rgba(0, 0, 0, 0.2);
     }
 
-    @media screen and (max-width: 1000px){
+    @media screen and (max-width: 900px){
         .menu{
+            flex-direction: column;
             justify-content: space-around;
+            position: fixed;
+            top: 0;
+            left: 0;
+            height: 100vh;
+            width: 100vw;
+            background: #041e42;
+            z-index: 3;
         }
 
         .menu .inletSites{
-            width: 100%;
+            text-align: left;
         }
-    }
 
-    @media screen and (max-width: 500px){
         .menu a{
-            margin: 10px 5px;
+            color: white;
+            font-size: 22px;
+            padding: 20px 0;
+            width: 90%;
+            background: rgba(255, 255, 255, 0.5);
+        }
+
+        .openMenu, .closeMenu{
+            display: flex;
         }
     }
 </style>
