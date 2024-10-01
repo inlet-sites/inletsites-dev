5d065dfebe278e89fce1b4203bbb46874b8f18a5
Lee Morgan <lee@leemorgan.dev>
2024-10-01 15:21:07 -0400

    Create client page.

---
+++ src/routes/our-work/+page.svelte
@@ -0,0 +1,62 @@
+<script>
+    import "../../global.css";
+    import Menu from "../../components/Menu.svelte";
+
+    import blacklistAero from "$lib/clients/blacklist-aero.webp";
+</script>
+
+<Menu/>
+
+<div class="container">
+    <h1>Our work</h1>
+    <h2>View some of the work done by Inlet Sites</h2>
+    <div class="divider"></div>
+
+    <div class="sites">
+        <a href="https://blacklist.aero" class="card">
+            <p>BLACKLIST.AERO from Air Justice</p>
+            <img src={blacklistAero} alt="Blacklist.aero logo">
+        </a>
+    </div>
+</div>
+
+<style>
+    .container{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 75px auto;
+        width: 90%;
+    }
+
+    h1{
+        font-size: 38px;
+    }
+
+    h2{
+        text-align: center;
+    }
+
+    .divider{
+        border-bottom: 2px solid white;
+        width: 100%;
+    }
+
+    a{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        color: white;
+        margin: 35px 0;
+        font-size: 28px;
+        text-decoration: none;
+        box-shadow: 0 0 5px white;
+        padding: 15px;
+        border-radius: 15px;
+    }
+
+    a p{
+        font-size: 14px;
+        margin-bottom: 15px;
+    }
+</style>
